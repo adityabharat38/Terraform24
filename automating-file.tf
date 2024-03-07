ec1642e00231bc90c422b37ea5ba73ae69d292b2
Ubuntu <ubuntu@ip-172-31-5-171.ap-south-1.compute.internal>
2024-03-07 20:02:22 +0000

    creating basic resource as type 'local-file' and executing Terraform to automate creation of a file

---
+++ automating-file.tf
@@ -0,0 +1,4 @@
+resource "local_file" "first-file" {
+	filename = "Meri-file.txt"
+	content = "Hi , Welcome to Terraform , when you will execute this tf file your file with name Merifile will be created and this content will be visible"
+	}
